7bd1e6a7018c849b833897059390d61284c7befe
WaithakaGuru <waithakaoffices@gmail.com>
2025-05-21 12:30:03 +0300

    Add Completed Challenge 3 'Perimeter of a Rectangle'
    - add code solution for Challenge 3

--- milestone_1_challenges.js
+++ milestone_1_challenges.js
@@ -9,6 +9,14 @@ function convert(numberOfMinutes) {
   return numberOfMinutes * 60;
 }
 
+// Challenge 3: Find Perimeter of a Rectangle
+function findRectanglePerimeter(length, width) {
+  // check for negatives or zero
+  if (length <= 0 || width <= 0)
+    return "length/width of a shape cannot be negative";
+  return 2 * (length + width);
+}
+
 /* Test Area */
 console.log("Test for addition of Two Numbers");
 console.log(addition(-3, 7)); // 4
@@ -19,3 +27,9 @@ console.log(addition(4, "r")); //arguments must be of type 'number'
 console.log("Test for Minutes to Seconds conversion");
 console.log(convert(90)); // 5400
 console.log(convert(60)); // 3600
+
+console.log("Test for Finding Perimeter of a Rectangle");
+console.log(findRectanglePerimeter(3, 5)); // 16
+console.log(findRectanglePerimeter(45, "y")); // NaN
+console.log(findRectanglePerimeter(-2, 3)); //  "length/width of a shape cannot be negative"
+console.log(findRectanglePerimeter(3, 0)); // "length/width of a shape cannot be negative"
