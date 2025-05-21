a2c71f20e97873b075e66bc15b2d768ecfc1ac75
WaithakaGuru <waithakaoffices@gmail.com>
2025-05-21 12:25:42 +0300

    Add Completed Challenge 2 "Convert Minutes into Seconds"
    - add code solution for Challenge 2

--- milestone_1_challenges.js
+++ milestone_1_challenges.js
@@ -4,9 +4,18 @@ function addition(num1, num2) {
   return "arguments must be of type 'number'";
 }
 
+// Challenge 2: Convert minutes into seconds
+function convert(numberOfMinutes) {
+  return numberOfMinutes * 60;
+}
+
 /* Test Area */
 console.log("Test for addition of Two Numbers");
 console.log(addition(-3, 7)); // 4
 console.log(addition(-2, -23)); // -25
 console.log(addition(45, 81)); // 126
 console.log(addition(4, "r")); //arguments must be of type 'number'
+
+console.log("Test for Minutes to Seconds conversion");
+console.log(convert(90)); // 5400
+console.log(convert(60)); // 3600
